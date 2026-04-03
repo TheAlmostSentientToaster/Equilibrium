8c1c1ffbae8029204935963672c84c4aa38423a8
TheAlmostSentientToaster <144105942+TheAlmostSentientToaster@users.noreply.github.com>
2026-04-03 23:35:09 +0200

    Defined adapters for REST-API for mobile App (#48)
    Co-authored-by: Tim Vogel <144105942+SocialMediaIsBad@users.noreply.github.com>

---
+++ adapters/mobile_app_adapters.py
@@ -0,0 +1,24 @@
+from application.ports import InputMessagePort, OutputMessagePort
+from domain.command import Command
+from domain.message import Message
+from domain.chat_context import ChatContext
+
+class MobileAppOutboundAdapter(OutputMessagePort):
+    async def send_messages(self, messages: list[Message], chat_context: ChatContext):
+        pass
+
+    async def send_broadcast(self, messages: list[Message], users: list[int]):
+        pass
+
+    async def send_image(self, image_path: str, chat_context: ChatContext):
+        pass
+
+class MobileAppInboundAdapter(InputMessagePort):
+    def receive_photo(self, photo_data: bytearray, user_id: int, user_name: str, chat_context: ChatContext):
+        pass
+
+    def receive_message(self, content: str, user_id: int, user_name: str, chat_context: ChatContext):
+        pass
+
+    def receive_command(self, command: Command, chat_context: ChatContext):
+        pass
